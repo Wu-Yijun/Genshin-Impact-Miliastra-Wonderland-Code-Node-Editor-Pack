377bb04c75c075e2a7088c82a0540e4ac2f5f089
Wu-Yijun <wuyijun21@mails.ucas.ac.cn>
2025-12-01 18:26:59 +0800

    update

---
+++ .github/actions/CI/gia_gen/readme.md
@@ -0,0 +1,11 @@
+## 测试 Graph 接口
+
+代码见 [test.ts](./test.ts).
+
+下图是官方教程中的示例, 以及最终生成结果的截图. 输出文件可以在 Actions Artifacts 中取得.
+
+*参考图:*
+![ref](./reference.png)
+
+*生成效果:*
+![gen](./generated.png)
--- .github/actions/CI/gia_gen/test.ts
+++ .github/actions/CI/gia_gen/test.ts
@@ -2,38 +2,95 @@ import { Graph } from "../../utils/gia_gen/index.ts";
 import { NODE_ID } from "../../utils/node_data/index.ts";
 import { encode_gia_file } from "../../utils/protobuf/decode.ts";
 
+const DSL = `
+[OnTab()[source=src, tab_id=id]].Switch(tab_id)(
+    1 = If(m.eq(source.Plant_Level, 0))
+      .CreatePrefab(1077236130, q.pos(), q.rot(), Self, level=1)
+      .SetVal(source.Plant_Level, 1, true),
+    2 = If(m.eq(Self.Plant_Level,0))
+      .CreatePrefab(1077236131, q.pos(), q.rot(), Self, level=1)
+      .SetVal(source.Plant_Level, 2, true),
+    3 = If(m.eq(Self.Plant_Level,0))
+      .PlayEffects(10002107, target=Self, "RootNode", true, true, zoom=0.1),
+  );
+`;
+console.log("The equivlent DSL is:", DSL);
+console.log("Creating Graph......");
 
 const graph = new Graph("server", undefined, "Github Actions CI Test Generated Graph");
 
 // column 1
-const trig = graph.add_node(NODE_ID.When_Tab_Is_Selected);
+const Trig = graph.add_node(NODE_ID.When_Tab_Is_Selected);
 // column 2
-const branch1 = graph.add_node(NODE_ID.Multiple_Branches__Int);
+const Branch1 = graph.add_node(NODE_ID.Multiple_Branches__Int);
 const get_val = graph.add_node(NODE_ID.Get_Custom_Variable__Int);
-graph.flow(trig, branch1);
-graph.connect(trig, branch1, 2, 0);
-graph.connect(trig, get_val, 0, 0);
-branch1.setVal(1, [1, 2, 3]);
+graph.flow(Trig, Branch1);
+graph.connect(Trig, Branch1, 2, 0);
+graph.connect(Trig, get_val, 0, 0);
+Branch1.setVal(1, [1, 2, 3]);
 get_val.setVal(1, "Plant Level");
 // column 3
-const branch2 = graph.add_node(NODE_ID.Double_Branch);
+const Branch2 = graph.add_node(NODE_ID.Double_Branch);
 const eq1 = graph.add_node(NODE_ID.Equal__Int);
-const branch3 = graph.add_node(NODE_ID.Double_Branch);
+const Branch3 = graph.add_node(NODE_ID.Double_Branch);
 const eq2 = graph.add_node(NODE_ID.Equal__Int);
-const branch4 = graph.add_node(NODE_ID.Double_Branch);
+const Branch4 = graph.add_node(NODE_ID.Double_Branch);
 const eq3 = graph.add_node(NODE_ID.Equal__Int);
-graph.flow(branch1, branch2, 1, 0);
-graph.flow(branch1, branch3, 2);
-graph.flow(branch1, branch4, 3);
+graph.flow(Branch1, Branch2, 1, 0);
+graph.flow(Branch1, Branch3, 2);
+graph.flow(Branch1, Branch4, 3);
 graph.connect(get_val, eq1, 0, 0);
 graph.connect(get_val, eq2, 0, 0);
 graph.connect(get_val, eq3, 0, 0);
-graph.connect(eq1, branch2, 0, 0);
-graph.connect(eq2, branch3, 0, 0);
-graph.connect(eq3, branch4, 0, 0);
+graph.connect(eq1, Branch2, 0, 0);
+graph.connect(eq2, Branch3, 0, 0);
+graph.connect(eq3, Branch4, 0, 0);
 eq1.setVal(1, 0);
 eq2.setVal(1, 1);
 eq3.setVal(1, 2);
+// column 4
+const slf1 = graph.add_node(NODE_ID.Get_Self_Entity);
+const loc1 = graph.add_node(NODE_ID.Get_Entity_Location_and_Rotation);
+const Pfb1 = graph.add_node(NODE_ID.Create_Prefab);
+const loc2 = graph.add_node(NODE_ID.Get_Entity_Location_and_Rotation);
+const Pfb2 = graph.add_node(NODE_ID.Create_Prefab);
+const slf2 = graph.add_node(NODE_ID.Get_Self_Entity);
+const Eff1 = graph.add_node(NODE_ID.Mount_Looping_Special_Effect);
+graph.flow(Branch2, Pfb1);
+graph.flow(Branch3, Pfb2);
+graph.flow(Branch4, Eff1);
+graph.connect(slf1, loc1, 0, 0);
+graph.connect(loc1, Pfb1, 0, 1);
+graph.connect(loc1, Pfb1, 1, 2);
+graph.connect(slf1, Pfb1, 0, 3);
+graph.connect(slf1, loc2, 0, 0);
+graph.connect(loc2, Pfb2, 0, 1);
+graph.connect(loc2, Pfb2, 1, 2);
+graph.connect(slf1, Pfb2, 0, 3);
+graph.connect(slf2, Eff1, 0, 1);
+Pfb1.setVal(0, 1077236130);
+Pfb1.setVal(6, 1);
+Pfb2.setVal(0, 1077236131);
+Pfb2.setVal(6, 1);
+Eff1.setVal(0, 10002107);
+Eff1.setVal(2, "RootNode");
+Eff1.setVal(3, true);
+Eff1.setVal(4, true);
+Eff1.setVal(7, 0.1);
+// column 5
+const set_val1 = graph.add_node(NODE_ID.Set_Custom_Variable__Int);
+const set_val2 = graph.add_node(NODE_ID.Set_Custom_Variable__Int);
+graph.flow(Pfb1, set_val1);
+graph.flow(Pfb2, set_val2);
+graph.connect(slf1, set_val1, 0, 0);
+graph.connect(slf1, set_val2, 0, 0);
+set_val1.setVal(1, "Plant Level");
+set_val1.setVal(2, 1);
+set_val1.setVal(4, true);
+set_val2.setVal(1, "Plant Level");
+set_val2.setVal(2, 2);
+set_val2.setVal(4, true);
+
 // auto analysis layout
-graph.autoLayout();
+graph.autoLayout(1, 2);
 encode_gia_file("./dist/GeneratedGraph.gia", graph.encode());
