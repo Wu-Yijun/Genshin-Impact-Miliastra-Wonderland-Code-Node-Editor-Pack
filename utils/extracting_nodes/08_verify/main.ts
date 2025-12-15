fffff09c9ea26e42efc34e94f218de01eb9d85e8
Wu-Yijun <wuyijun21@mails.ucas.ac.cn>
2025-12-15 11:14:47 +0800

    add test1

--- utils/extracting_nodes/08_verify/main.ts
+++ utils/extracting_nodes/08_verify/main.ts
@@ -1,14 +1,17 @@
-import { ClientVarType, NodeConnection, VarBase_Class, VarBase_ItemType_ClassBase, type GraphNode, type NodePin } from "../../protobuf/gia.proto.ts";
+import type { ClientVarType, NodeConnection, GraphNode, NodePin } from "../../protobuf/gia.proto.ts";
+import { VarBase_Class, VarBase_ItemType_ClassBase } from "../../protobuf/gia.proto.ts";
 import { collection, dir, gia, save } from "../util.ts";
+import { assert, assertDeepEq, assertEq, exclude_keys } from "../../utils.ts";
 import { is_reflect } from "../../gia_gen/nodes.ts";
 
 import RECORDS from "../dist/node_records.json" with {type: "json"};
 import type_name from "../dist/type_ids.json" with { type: 'json' };
-import { assert } from "../../utils";
+import NO_LINK from "../dist/node_pins_no_links.json" with { type: 'json' };
 
-// 1. 对于每一个 node, 尽可能连接全部的引脚, 或设置初值(reflective pin除外). 观测是否完全连接, 然后验证输入输出类型是否完全一致
+// 1. 对于每一个 node, 尽可能连接全部的引脚, 或设置初值(reflective pin除外). 观测是否完全连接(或被标记无连接), 然后验证输入输出类型是否完全一致.
 // 2. 对于每一个 reflective node, 构造全部的反射类型, 并连接/设置初值, 然后验证输入输出类型是否完全一致
-// 3. 对于每一个 enum 类型, 设置每一种可能的值, 然后验证输入输出值是否一致
+// 3. 对于每一个 enum 类型, 设置每一种可能的值, 然后验证输入输出值是否一致(或有固定值)
+// 4. 对于每一个 simple 类型, 设置多个值, 然后验证输入输出值是否一致(或有固定值)
 
 
 const info = collection().find(x => x.name === "skill")!;
@@ -28,8 +31,8 @@ function create_node(index: number, gid: number, cid?: number, pins?: NodePin[],
       nodeId: cid
     },
     pins: pins ?? [],
-    x: x_pos,
-    y: y_pos,
+    x: pos[0],
+    y: pos[1],
     usingStruct: []
   };
 }
@@ -128,26 +131,74 @@ function test1(read = false) {
   const graph = gia("skill");
   const records = RECORDS.filter(x => !x.name.startsWith("Node Graph End"));
   const nodes = graph.graph.graph?.inner.graph.nodes!;
+  // let nodes_read = read ? gia("all_nodes", true, true).graph.graph?.inner.graph.nodes! : [];
+  let nodes_read = read ? gia("../dist/all_nodes", undefined, true).graph.graph?.inner.graph.nodes! : [];
   let y_pos = 0;
   let index = 1;
+  let read_index = 0;
+  let read_node = nodes[0];
   records.slice(0, 5).forEach((rec, i) => {
     const pins: NodePin[] = [];
-    nodes.push(create_node(index, rec.id, undefined, pins, [0, y_pos]));
+    const index0 = index++;
+    nodes.push(create_node(index0, rec.id, undefined, pins, [0, y_pos]));
+    if (read) {
+      read_node = nodes_read[read_index++];
+      assertEq(read_node.genericId.nodeId, rec.id);
+    }
     for (let i = 0; i < rec.inputs.length; i++) {
       if (is_reflect(rec.inputs[i])) continue;
       const get_var = get_var_node(rec.inputs[i], index++, [-500 - i * 200, y_pos]);
       if (get_var === null) continue;
       nodes.push(get_var.node);
+      const type_id = type_name.find(x => x.type === rec.inputs[i])!.typeid;
       pins.push({
         i1: { kind: 3, index: i },
         i2: { kind: 3, index: 0 },
         value: {} as any,
-        type: type_name.find(x => x.type === rec.inputs[i])!.typeid,
+        type: type_id,
         connects: [get_var.connect]
       });
+      if (read) {
+        const read_node_get = nodes_read[read_index++];
+        assertEq(read_node_get.pins.length, 1); // should have a out pin
+        assertEq(read_node_get.pins[0].type, type_id); // type should match
+        if (NO_LINK.find(x => x.node === read_node.genericId.nodeId && x.pin === i)) {
+          assertEq(read_node_get.pins[0].connects.length, 0); // should be not connected to it
+          continue;
+        }
+        assertEq(read_node.pins.find(x => x.i1.index === i)?.connects.length, 1); // should be connected to it
+        assertDeepEq(
+          exclude_keys(read_node.pins.find(x => x.i1.index === i), "value"),
+          exclude_keys(pins[pins.length - 1], "value")
+        ); // should be same  
+      }
+    }
+    for (let i = 0; i < rec.outputs.length; i++) {
+      if (is_reflect(rec.outputs[i])) continue;
+      const set_var = set_var_node(rec.outputs[i], index++, [index0, i], [500 + i * 200, y_pos]);
+      if (set_var === null) continue;
+      nodes.push(set_var);
+      if (read) {
+        const read_node_set = nodes_read[read_index++];
+        const type_id = type_name.find(x => x.type === rec.outputs[i])!.typeid;
+        assertEq(read_node_set.pins.length, 1); // should have a in pin
+        assertEq(read_node_set.pins[0].type, type_id); // type should match
+        assertEq(read_node_set.pins[0].connects.length, 1); // should be connected to it
+        assertDeepEq(
+          exclude_keys(read_node_set.pins[0], "value"),
+          exclude_keys(set_var.pins[0], "value")
+        ); // should be same
+      }
     }
     y_pos += 100 + 30 * rec.inputs.length;
   });
-  save("outputs_int.gia", graph, true);
-  console.log("outputs_int.gia saved with", nodes.length, "nodes");
+  if (!read) {
+    save("all_nodes.gia", graph, false);
+    console.log("all_nodes.gia saved with", nodes.length, "nodes");
+  } else {
+    console.log("all_nodes.gia verified with", nodes.length, "nodes");
+  }
 }
+
+test1(false);
+test1(true);
--- utils/utils.ts
+++ utils/utils.ts
@@ -15,6 +15,28 @@ export function todo<T>(msg?: string): T {
   return 0 as any;
 }
 
+export function exclude_keys(obj: any, ...keys: (string | string[])[]): any {
+  function remove_key(o: any, key: string[]) {
+    if (key.length === 1) {
+      delete o[key[0]];
+    } else {
+      const k = key[0];
+      if (o[k] === undefined) return;
+      remove_key(o[k], key.slice(1));
+    }
+  }
+  const ret = structuredClone(obj);
+  for (const key of keys) {
+    if (typeof key === "string") {
+      delete ret[key];
+    } else {
+      remove_key(ret, key);
+    }
+  }
+  return ret;
+}
+
+
 export function assert(cond: boolean, msg?: string): asserts cond {
   if (cond) return;
   throw new Error(msg ?? "Assertion failed");
@@ -23,6 +45,25 @@ export function assertEq<T>(target: unknown, expect: T): asserts target is T {
   if (target === expect) return;
   console.error("[Assertion]", target, "!==", expect);
   throw new Error("Assertion failed");
+}
+export function assertDeepEq<T>(target: T, expect: T): asserts target is T {
+  const isObject = (v: any) => v && typeof v === "object";
+  function deepEqual(a: any, b: any): boolean {
+    if (a === b) return true;
+    if (isObject(a) && isObject(b)) {
+      const keysA = Object.keys(a);
+      const keysB = Object.keys(b);
+      if (keysA.length !== keysB.length) return false;
+      for (const key of keysA) {
+        if (!deepEqual(a[key], b[key])) return false;
+      }
+      return true;
+    }
+    return false;
+  }
+  if (deepEqual(target, expect)) return;
+  console.error("[Assertion]", target, "not deep equal to", expect);
+  throw new Error("Deep Assertion failed");
 }
 export function assertEqs<const T extends readonly any[]>(target: unknown, ...expects: T): asserts target is T[number] {
   if (expects.some((v) => v === target)) return;
